9e18c10dc7923c72e1ceedc51d6a09792fd8afa5
AmanApT <amanasstudent@gmail.com>
2024-02-11 21:35:50 +0530

    Circular Text

--- components/About.tsx
+++ components/About.tsx
@@ -22,7 +22,31 @@ const About = () => {
           <Image className="about-image" src={aboutImg} alt="owner" />
         </div>
       </div>
+
+      <div className="rounded-text rotating">
+        <svg viewBox="0 0 200 200">
+          <path
+            id="textPath"
+            d="M 85,0 A 85,85 0 0 1 -85,0 A 85,85 0 0 1 85,0"
+            transform="translate(100,100)"
+            fill="none"
+            stroke-width="0"
+          ></path>
+          <g font-size="17.1px">
+            <text text-anchor="start">
+              <textPath
+                className="coloring"
+                xlinkHref="#textPath"
+                startOffset="0%"
+              >
+                -SAUMYA PANDEY -- CONTENT WRITER- saumyapandey@gmail.com
+              </textPath>
+            </text>
+          </g>
+        </svg>
+      </div>
       <Image src={hrSvg} alt="horizontal line" className="about-hr" />
+
       {/* <div className="rotating-text-container">
         <p className="rotating-text">CAT WOMAN • PLANT LADY • MARVEL GEEK</p>
       </div> */}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,26 +5,14 @@ import flowerBushes from "@/assets/heroBushes.png";
 import kaliPatti from "@/assets/kaliPatti.png";
 import heroFlowers from "@/assets/heroFlowers2.png";
 import textSvg from "@/assets/hero-text.svg";
+import heroSvg from "@/assets/svg-hero.svg";
 
 const Hero = () => {
   return (
     <div className="hero-section">
       <Nav />
-      {/* <div className="heroFlowers">
-        <h1>SAUMYA PANDEY</h1>
-      </div> */}
-      {/* <div>
-        <Image
-          style={{
-            width: "50%",
-            position: "absolute",
-            bottom: "-350px",
-            left: "350px",
-          }}
-          src={textSvg}
-          alt="text-svg"
-        />
-      </div> */}
+      <h1 className="hero-name">SAUMYA PANDEY</h1>
+
       <Image className="heroBushes" src={flowerBushes} alt="heroflowers" />
       <Image className="kaliPatti" src={kaliPatti} alt="heroBush" />
       <Image className="heroFlowers" src={heroFlowers} alt="heroBush" />
